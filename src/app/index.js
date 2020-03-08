5ea03ce76add7b0a1dba83f2cef024402d71ff5b
svb.bel85 <svb.bel85@gmail.com>
2020-03-08 18:09:41 +0200

    three commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,21 @@
 import '../style/main.scss';
+
+let email = document.getElementById('mail');
+let error = document.querySelector('.error');
+
+email.addEventListener("focus", function (event) {
+    error.innerHTML = "";
+    error.className = "error";
+    email.className = 'form-input'
+
+}, true);
+
+email.addEventListener("blur", function (event) {
+    const email_input = email.value;
+
+    if(!email_input.includes('@')){
+        error.innerHTML = "Please enter a valid email address";
+        error.className = "error active";
+        email.className = 'form-input_error'
+    }
+}, true);
