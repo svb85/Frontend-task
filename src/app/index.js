a407c2ac534bc9cd806265e3a7f8aca75914bcb0
Serg <svb.bel85@gmail.com>
2020-03-09 16:01:40 +0200

    six commit

--- src/app/index.js
+++ src/app/index.js
@@ -4,27 +4,26 @@ let data = new Object();
 
 let form  = document.getElementsByTagName('form')[0];
 let error = document.querySelector('.error');
-let select = document.getElementsByClassName('items');
 let button = document.getElementById('button_next');
 
 form.addEventListener("focus", function (event) {
     const elem = event.target;
+
     elem.addEventListener("input", function (event) {
-        enabled_button(data,button);
-        if(elem.type == 'text') {
+        if (elem.type == 'text') {
             let val = elem.value;
-            if(!val.length == 0) {
+            if (!val.length == 0) {
                 data.name = elem.value;
-            }else{
+            } else {
                 data.name = undefined;
             }
         }
 
-        if(elem.type == 'password') {
+        if (elem.type == 'password') {
             let val = elem.value;
-            if (!val.length == 0) {
+            if (val.length >= 8) {
                 data.pass = elem.value;
-            }else{
+            } else if(val.length <= 9 ){
                 data.pass = undefined;
             }
         }
@@ -41,18 +40,36 @@ form.addEventListener("focus", function (event) {
                     error.innerHTML = "";
                     error.className = "error";
                     elem.className = 'form-input';
-                    elem.nextElementSibling.className = '';
+                    elem.nextElementSibling.className = 'normal_email';
                     data.email = elem.value;
                 }
             }else{
                 data.email = undefined;
             }
         }
-    });
+
+        enabled_button(data, button);
+    }, true);
+}, true);
+
+form.addEventListener("change", function (event) {
+    const select = event.target;
+
+    if(select.type == 'radio'){
+        console.log(select.type);
+        let label = select.nextElementSibling.innerText
+        if(!label.length == 0){
+            console.log(label);
+            data.select = label;
+        }else{
+            data.select = undefined
+        }
+        enabled_button(data,button);
+    }
 }, true);
 
 function enabled_button(data,button) {
-    if( data.name !== undefined && data.email !== undefined && data.pass !== undefined){
+    if( data.name !== undefined && data.email !== undefined && data.select !== undefined && data.pass !== undefined ){
         button.className = 'form-button';
     }else{
         button.className = 'form-button_disabled'
