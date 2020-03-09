15ea932fbdcad13da938462f68ad2ad26a082c05
Serg <svb.bel85@gmail.com>
2020-03-09 02:52:53 +0200

    four commit

--- src/app/index.js
+++ src/app/index.js
@@ -1,21 +1,67 @@
 import '../style/main.scss';
 
-let email = document.getElementById('mail');
+let data = new Object();
+
+let form  = document.getElementsByTagName('form')[0];
 let error = document.querySelector('.error');
+let select = document.getElementsByClassName('items');
+let button = document.getElementById('button_next');
 
-email.addEventListener("focus", function (event) {
-    error.innerHTML = "";
-    error.className = "error";
-    email.className = 'form-input'
+form.addEventListener("focus", function (event) {
+    const elem = event.target;
 
+    elem.addEventListener("input", function (event) {
+        enabled_button(data,button);
+
+        if(elem.type == 'text') {
+            let val = elem.value;
+            if(!val.length == 0) {
+                data.name = elem.value;
+            }else{
+                data.name = undefined;
+            }
+        }
+
+        if(elem.type == 'password') {
+            let val = elem.value;
+            if (!val.length == 0) {
+                data.pass = elem.value;
+            }else{
+                data.pass = undefined;
+            }
+        }
+
+        if(elem.type == 'email') {
+            let val = elem.value;
+            if(!val.length == 0) {
+                if (!elem.value.includes('@')) {
+                    error.innerHTML = "Please enter a valid email address";
+                    error.className = "error active";
+                    elem.className = 'form-input_error';
+                    elem.nextElementSibling.className= 'error_label';
+                } else {
+                    error.innerHTML = "";
+                    error.className = "error";
+                    elem.className = 'form-input';
+                    elem.nextElementSibling.className = '';
+                    data.email = elem.value;
+                }
+            }else{
+                data.email = undefined;
+            }
+        }
+
+        console.log(data.name,data.email,data.pass);
+    });
 }, true);
 
-email.addEventListener("blur", function (event) {
-    const email_input = email.value;
-
-    if(!email_input.includes('@')){
-        error.innerHTML = "Please enter a valid email address";
-        error.className = "error active";
-        email.className = 'form-input_error'
+function enabled_button(data,button) {
+    console.log(data,button);
+    if( data.name !== undefined && data.email !== undefined && data.pass !== undefined){
+        button.className = 'form-button';
+    }else{
+        button.className = 'form-button_disabled'
     }
-}, true);
+}
+
+console.log(data.name,data.email,data.pass);
